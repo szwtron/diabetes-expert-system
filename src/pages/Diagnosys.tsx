d91da27fbe57e6ede0203c55d861c13988ac6884
Stainley <szwtron@gmail.com>
2021-11-09 18:04:59 +0700

    Initial FE setup

---
+++ src/pages/Diagnosys.tsx
@@ -0,0 +1,92 @@
+import { IonButton, IonCard, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonRow, IonTitle, IonToolbar } from "@ionic/react";
+import './Home.css';
+
+const Diagnosys = () => {
+  return (
+    <IonPage>
+        <IonHeader>
+            <IonToolbar>
+                <IonTitle>Diagnosys</IonTitle>
+            </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+            <IonHeader collapse="condense">
+                <IonToolbar>
+                    <IonTitle size="small">Diagnosys</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <section className="hero">
+                <IonCard>
+                    <div className="container">
+                        <div className="hero-inner">
+                            <IonGrid>
+                                <IonRow>
+                                <IonCol size="6">
+                                    <div>
+                                        <IonItem>
+                                            <IonLabel position="floating">Gula Darah Puasa (mg/dl)</IonLabel>
+                                            <IonInput></IonInput>
+                                        </IonItem>
+                                        <IonItem>
+                                            <IonLabel position="floating">Gula Plasma Puasa (mg/dl)</IonLabel>
+                                            <IonInput></IonInput>
+                                        </IonItem>
+                                        <IonItem>
+                                            <IonLabel position="floating">Gula Plasma Tidur (mg/dl)</IonLabel>
+                                            <IonInput></IonInput>
+                                        </IonItem>
+                                        <IonItem>
+                                            <IonLabel position="floating">Gula Darah 2 jam PP (mg/dl)</IonLabel>
+                                            <IonInput></IonInput>
+                                        </IonItem>
+                                        <IonItem>
+                                            <IonLabel position="floating">Kadar HbA1c (mg/dl)</IonLabel>
+                                            <IonInput></IonInput>
+                                        </IonItem>
+                                        <IonItem>
+                                            <IonLabel position="floating">Kadar HDL (mg/dl)</IonLabel>
+                                            <IonInput></IonInput>
+                                        </IonItem>
+                                        <IonItem>
+                                            <IonLabel position="floating">Kadar Trigliserida (mg/dl)</IonLabel>
+                                            <IonInput></IonInput>
+                                        </IonItem>
+                                        <IonItem>
+                                            <IonLabel position="floating">Kadar Insulin (%)</IonLabel>
+                                            <IonInput></IonInput>
+                                        </IonItem>
+                                        <IonItem>
+                                            <IonButton>Submit</IonButton>
+                                        </IonItem>
+                                    </div>
+                                </IonCol>
+                                <IonCol size="6">
+                                    <IonGrid>
+                                        <IonRow>
+                                            <IonCol>
+                                                <IonCardTitle></IonCardTitle>
+                                            </IonCol>
+                                            <IonCol>
+                                                <IonCardTitle>Gula Darah</IonCardTitle>
+                                            </IonCol>
+                                            <IonCol>
+                                                <IonCardTitle>Gula Darah</IonCardTitle>
+                                            </IonCol>
+                                            <IonCol>
+                                                <IonCardTitle>Gula Darah</IonCardTitle>
+                                            </IonCol>
+                                        </IonRow>
+                                    </IonGrid>
+                                </IonCol>
+                                </IonRow>
+                            </IonGrid>
+                        </div>
+                    </div>
+                </IonCard>
+            </section>
+        </IonContent>
+    </IonPage>
+  );
+};
+
+export default Diagnosys;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
@@ -7,16 +7,43 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="small">Home</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <section className="hero">
+          <div className="container">
+            <div className="hero-inner">
+              <div className="hero-copy">
+                <h1 className="hero-title mt-0">Expert System for Diabetes Diagnosys </h1>
+                <p className="hero-paragraph">Diagnose whether you are suffering from diabetes or not in less than 10 minutes</p>
+                <div className="hero-cta">
+                  <IonButton color="primary" href="/diagnosys">Diagnosys</IonButton>
+                </div>
+              </div>
+              <div className="hero-figure anime-element">
+                <svg className="placeholder" width="528" height="396" viewBox="0 0 528 396">
+                  <rect width="528" height="396"/>
+                </svg>
+                <div className="hero-figure-box hero-figure-box-01" data-rotation="45deg"></div>
+                <div className="hero-figure-box hero-figure-box-02" data-rotation="-45deg"></div>
+                <div className="hero-figure-box hero-figure-box-03" data-rotation="0deg"></div>
+                <div className="hero-figure-box hero-figure-box-04" data-rotation="-135deg"></div>
+                <div className="hero-figure-box hero-figure-box-05"></div>
+                <div className="hero-figure-box hero-figure-box-06"></div>
+                <div className="hero-figure-box hero-figure-box-07"></div>
+                <div className="hero-figure-box hero-figure-box-08" data-rotation="-22deg"></div>
+                <div className="hero-figure-box hero-figure-box-09" data-rotation="-52deg"></div>
+                <div className="hero-figure-box hero-figure-box-10" data-rotation="-50deg"></div>
+              </div>
+            </div>
+          </div>
+        </section>
       </IonContent>
     </IonPage>
   );
