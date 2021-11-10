4175e911329d7efb3cbc10b3a184b3a5cc41567b
Irena <tania.irena@student.umn.ac.od>
2021-11-10 15:36:27 +0700

    home image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -27,10 +27,10 @@ const Home: React.FC = () => {
                 </div>
               </div>
               <div className="hero-figure anime-element">
-                <svg className="placeholder" width="528" height="396" viewBox="0 0 528 396">
+                {/* <svg className="placeholder" width="528" height="396" viewBox="0 0 528 396">
                   <rect width="528" height="396"/>
-                </svg>
-                <div className="hero-figure-box hero-figure-box-01" data-rotation="45deg"></div>
+                </svg> */}
+                {/* <div className="hero-figure-box hero-figure-box-01" data-rotation="45deg"></div>
                 <div className="hero-figure-box hero-figure-box-02" data-rotation="-45deg"></div>
                 <div className="hero-figure-box hero-figure-box-03" data-rotation="0deg"></div>
                 <div className="hero-figure-box hero-figure-box-04" data-rotation="-135deg"></div>
@@ -39,7 +39,8 @@ const Home: React.FC = () => {
                 <div className="hero-figure-box hero-figure-box-07"></div>
                 <div className="hero-figure-box hero-figure-box-08" data-rotation="-22deg"></div>
                 <div className="hero-figure-box hero-figure-box-09" data-rotation="-52deg"></div>
-                <div className="hero-figure-box hero-figure-box-10" data-rotation="-50deg"></div>
+                <div className="hero-figure-box hero-figure-box-10" data-rotation="-50deg"></div> */}
+                <img width="528" height="396" src="assets/img/doctor.png"></img>
               </div>
             </div>
           </div>
